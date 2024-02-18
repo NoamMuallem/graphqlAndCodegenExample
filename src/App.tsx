311fd804a6242235b85cb897dfbb70856ea14046
Noam Muallem <noammu072@gmail.com>
2024-02-18 12:28:05 +0200

    added full type for graphql requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from "@apollo/client";
 import { GET_LAUNCHES } from "./queries/launchesPast";
+import { GetLaunchesQueryVariables, GetLaunchesQuery } from "./gql/graphql";
 
 function App() {
-  const { loading, error, data, refetch } = useQuery(GET_LAUNCHES, {
+  const { loading, error, data, refetch } = useQuery<GetLaunchesQuery, GetLaunchesQueryVariables>(GET_LAUNCHES, {
     onError: (error) => console.error("Error fetching launches:", error),
     onCompleted: (data) => console.log("Launch data:", data),
   });
@@ -11,20 +12,24 @@ function App() {
     return <p>Loading...</p>;
   }
 
-  if (error) {
+  if (error || !data?.launchesPast) {
     return <p>Error :(</p>;
   }
 
   return (
-    <div>
+    <div style={{display:"flex", justifyContent:"center", alignItems:"center", flexDirection:"column", width:"100%"}}>
       <h1>SpaceX Launches</h1>
       <button onClick={() => refetch()}>Refetch</button>
-      {data.launchesPast.map((launch) => (
-        <div key={launch.id}>
+      {data.launchesPast.map((launch) => {
+        if(!launch) return null
+
+        return (
+        <div key={launch.mission_name}>
           <h3>{launch.mission_name}</h3>
           <p>{new Date(launch.launch_date_local).toLocaleString()}</p>
         </div>
-      ))}
+      )
+      })}
     </div>
   );
 }
